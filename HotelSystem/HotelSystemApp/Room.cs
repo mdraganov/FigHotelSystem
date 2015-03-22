dcba5b04ebc63bfe387a1de54bca3cbdee10c0f1
Mihail Seykov <mihailseikov@gmail.com>
2015-03-22 14:38:41 +0200

    Revert "Classes separated in folders."
    This reverts commit 3027011c244c96f317f4392200ff487b239f15cd.

---
+++ HotelSystem/HotelSystemApp/Room.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HotelSystemApp
+{
+    public abstract class Room : IAvailable, IFeatures, IPrice
+    {
+        public int NumberOfRoom { get; set; }
+        public int NumberOfBeds { get; set; }
+
+        public bool IsAvailable
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public List<Features> AllFeaturesInRoom
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void AddFeature(Features someFeature)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveFeature(Features someFeature)
+        {
+            throw new NotImplementedException();
+        }
+        
+
+        public void CalculatePrice()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HotelSystem/HotelSystemApp/Services.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HotelSystemApp
+{
+    public abstract class Service : IPrice
+    {
+
+        public decimal Price
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void CalculatePrice()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
